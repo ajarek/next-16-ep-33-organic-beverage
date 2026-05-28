d5195d6d897566ee811ba40d26a44113f2525c50
ajarek <ajarek@poczta.onet.pl>
2026-05-29 00:34:38 +0200

    feat: add CartItem component to display product details in the cart

--- components/cart-item.tsx
+++ components/cart-item.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
 import type {Product} from '@/types/typeProduct'
+import Image from 'next/image'
+
 const CartItem = ({ item }: { item: Product }) => {
   return (
-    <div className="flex items-center gap-4">
+    <div className="w-full flex items-center gap-4">
+      <div className='w-auto h-40 relative'>
+      <Image src={item.image} alt={item.name} fill={true} className='object-cover 'sizes="(max-width: 768px) 100vw, 33vw" />
+      </div>
         <p>{item.name}</p>
         <p>{item.quantity}</p>
         <p>{item.price}</p> 
